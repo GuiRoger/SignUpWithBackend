01a5fd1e9392e6154a42b114170c48121a44ce1e
Guilherme Roger <guilhermerogerbergamini@gmail.com>
2022-03-14 18:07:07 -0300

    First Commit

---
+++ src/Js/Signup.js
@@ -0,0 +1,14 @@
+
+
+const urlParams = new URLSearchParams(window.location.search);
+
+var userdatas = {
+    firstname: urlParams.get('firstname')
+}
+console.log(userdatas.firstname)
+
+function SignUp(userdatas){
+    
+
+
+}
